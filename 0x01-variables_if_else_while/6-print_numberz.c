7120d361ca281dd6a620fbf7d14ebdb1245592fc
ISAIAH-max <mlewaisaiah@gmail.com>
2020-09-18 23:55:20 +0300

    Program that print base 10 numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main - function that print base 10 numbers
+ *Return: Zero
+ */
+
+int main(void)
+{
+int n;
+while (n < 10)
+{
+putchar(n);
+n++;
+}
+putchar('\n');
+
+return (0);
+}
