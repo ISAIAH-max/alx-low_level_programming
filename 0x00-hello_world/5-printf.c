9a08a3cee9395fe26d06eb247b793baf6c176fb3
ISAIAH-max <mlewaisaiah@gmail.com>
2020-09-18 19:12:28 +0300

    C program print String using printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+/*Program that prints given String*/
+#include <stdio.h>
+/*main funtion*/
+/*return- 0*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
