fc0255f6243ee9a7c37f261cc558cd0f77f9b364
ISAIAH-max <mlewaisaiah@gmail.com>
2020-09-18 23:45:22 +0300

    Program that print base 10 numbers 1-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *main - function that prints base 10 numbers
+ *Return: Zero
+ */
+
+int main(void)
+{
+int n;
+char s = '\n';
+
+for (n = 1; n < 10; n++)
+{
+printf("%d", n);
+}
+putchar(s);
+
+return (0);
+
+}
