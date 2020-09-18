8a36045c55541cc9583d8f89cf0ec38f27509169
ISAIAH-max <mlewaisaiah@gmail.com>
2020-09-18 03:55:40 +0300

    C program that prints a string

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
-main() {
-puts(Programming is like building a multilingual puzzle);
-return 0;
+main()
+{
+puts("Programming is like building a multilingual puzzle");
+return (0);
 }
