5d05748eed6743c261bb700fc3fd80cce4ac7a9e
ISAIAH-max <mlewaisaiah@gmail.com>
2020-09-18 20:18:10 +0300

    C program that print given String

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
-/*program to print a String*/
 #include <stdio.h>
-/*main function begins here*/
+/**
+ *main - function begins here
+ *puts - Prints String on the screen
+ *return - zero
+*/
 int main(void)
 {
-/*Function puts() used to print the String*/
 puts("\"Programming is like building a multilingual puzzle");
-/*return function that returns zero value and terminates the program*/
 return (0);
 }
