d5185e4ccd6b419681dd1856ee70ecccdd02a738
ISAIAH-max <mlewaisaiah@gmail.com>
2020-09-18 19:37:42 +0300

    C program that print sizes of data types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,10 +3,10 @@
 /*main function*/
 int main(voin)
 {
-printf("Size of a char: %lu byte(s)\n", sizeof(char));
-printf("Size of a int: %lu byte(s)\n", sizeof(int));
-printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
-printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
-printf("Size of a float: %lu byte(s)\n", sizeof(float));
+printf("Size of a char: %c byte(s)\n", sizeof(char));
+printf("Size of a int: %d byte(s)\n", sizeof(int));
+printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %lld byte(s)\n", sizeof(long long int));
+printf("Size of a float: %f byte(s)\n", sizeof(float));
 return (0);
 }
