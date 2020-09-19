1401736d0752601eebf26db697d4138e32f02627
ISAIAH-max <mlewaisaiah@gmail.com>
2020-09-20 00:31:31 +0300

    A program that prints base 16 numbers in lowercase

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- *main - function that prints the base 16 numbers in lowercase
+ *main - function that prints base 16 numbers in lowercase
  *Return: Zero
  */
 
@@ -13,7 +13,7 @@ for (x = 48; x <= 57; x++)
 putchar(x);
 }
 int y;
-for (y = 'a'; y <= 'f'; y++)
+for (y = 97; y <= 102; y++)
 {
 putchar(y);
 }
