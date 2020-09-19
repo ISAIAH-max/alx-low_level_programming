dee7f71288a5be2f51bd7031227b72eddfdedeca
ISAIAH-max <mlewaisaiah@gmail.com>
2020-09-20 02:11:22 +0300

    Program that prints base 16 numbers

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,15 +8,16 @@
 int main(void)
 {
 int x;
+int y;
 for (x = 48; x <= 57; x++)
 {
 putchar(x);
 }
-int y;
-for (x = 97; x <= 102; x++)
+for (y = 97; y <= 102; y++)
 {
-putchar(x);
+putchar(y);
 }
+
 putchar('\n');
 return (0);
 }
