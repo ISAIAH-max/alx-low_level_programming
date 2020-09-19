2f27df48e86cf1d4a6c1e02cc3f262b93a203494
ISAIAH-max <mlewaisaiah@gmail.com>
2020-09-20 02:56:24 +0300

    A program that prints the numbers from 00 to 99

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ *main - function that prints the numbers from 00 to 99
+ *Return: Zero
+ */
+
+int main(void)
+{
+int x, y;
+for (x = 48; x <= 57; x++)
+{
+for (y = 48; y <= 57; y++)
+{
+putchar(x);
+putchar(y);
+putchar(' ');
+if (x <= 57 && y < 57)
+{
+putchar(',');
+}
+}
+}
+putchar('\n');
+
+return (0);
+}
