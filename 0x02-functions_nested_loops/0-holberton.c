cda98ddb34dcca74177e3ce33d09fef15e0f1742
ISAIAH-max <mlewaisaiah@gmail.com>
2020-09-22 02:05:03 +0300

    A program that print Holberton

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ *main - Program that prints Holberton
+ *Return: Always zero(success)
+ */
+
+int main(void)
+{
+char *school_name = "Holberton";
+int letters = 0;
+for (letters = 0; letters <= 9; letters++)
+{
+_putchar(school_name[letters]);
+}
+_putchar('\n');
+
+return (0);
+}
