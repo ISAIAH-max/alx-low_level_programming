ecd0ea6275d199de1a943c71784ae19a881a9a37
ISAIAH-max <mlewaisaiah@gmail.com>
2020-09-25 01:06:48 +0300

    A function that prints a tringle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+
+/**
+ *print_triangle - A function that prints a triangle
+ *@size: the size of the triangle
+ *
+ */
+
+void print_triangle(int size)
+{
+int x, y, z;
+
+x = 0;
+y = size - 1;
+while (x < size)
+{
+y = size - 1 - x;
+z = x + 1;
+
+while (y > 0)
+{
+_putchar(' ');
+y--;
+}
+while (z > 0)
+{
+_putchar('#');
+z--;
+}
+_putchar('\n');
+x++;
+}
+if (size <= 0)
+_putchar('\n');
+}
