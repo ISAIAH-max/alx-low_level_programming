e137b02d36b281414230cf6e75db0fcfa3a17795
ISAIAH-max <mlewaisaiah@gmail.com>
2020-09-25 01:24:27 +0300

    A program that prints Fizz and Buzz within 0 and 100

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,17 +16,17 @@ int x;
 x = 1;
 while (x <= 100)
 {
-if (x % 3 == 0)
+if (x % 3 == 0 && x % 5 == 0)
+{
+printf("FizzBuzz");
+}
+else if (x % 3 == 0)
 {
 printf("Fizz");
 }
 else if (x % 5 == 0)
 {
 printf("Buzz");
-}
-else if (x % 3 == 0 && x % 5 == 0)
-{
-printf("FizzBuzz");
 }
 else
 {
