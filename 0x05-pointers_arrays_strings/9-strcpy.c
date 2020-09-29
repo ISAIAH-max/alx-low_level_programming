144fbe1ceab9e93185a483e7c8d783bd593df32b
ISAIAH-max <mlewaisaiah@gmail.com>
2020-09-29 23:08:27 +0300

    Function that copies the string pointed to the buffer pointer

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * *_strcpy - A function that copies the string pointed to
+ * by src, including the terminating null byte (\0), to
+ * the buffer pointed to by dest
+ * @dest: String copied
+ * @src:  String to be copied
+ * Return: @dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+int x;
+x = 0;
+while (src[x])
+{
+dest[x] = src[x];
+x++;
+}
+return (dest);
+}
