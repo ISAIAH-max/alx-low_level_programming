b2de02cba344d3670be7807f4883acd1a6b8f90e
ISAIAH-max <mlewaisaiah@gmail.com>
2020-09-29 16:50:05 +0300

    A function that prints a string, in reverse, followed by a new line

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ *print_rev -  function that prints a string, in reverse,
+ *followed by a new line
+ *@s: The string used
+ */
+
+void print_rev(char *s)
+{
+int forward, reverse;
+
+for (forward = 0; s[forward] != '\0'; forward++)
+{
+
+}
+for (reverse = forward - 1; reverse >= s[forward]; reverse--)
+{
+_putchar(s[reverse]);
+}
+_putchar('\n');
+}
