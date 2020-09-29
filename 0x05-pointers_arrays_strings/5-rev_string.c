b0a51ccf701860165291fcc899df7c5511c3973b
ISAIAH-max <mlewaisaiah@gmail.com>
2020-09-29 18:06:02 +0300

    function that reverses a string.

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ *rev_string - function that reverses a string
+ *@s: String used
+ *
+ */
+
+void rev_string(char *s)
+{
+int forward, reverse, temp;
+
+while(s[forward] != '\0')
+{
+forward++;
+}
+forward--;
+
+for (reverse = 0; reverse < forward; reverse++, forward--)
+{
+temp = s[forward];
+s[forward] = s[reverse];
+s[reverse] = temp;
+}
+}
