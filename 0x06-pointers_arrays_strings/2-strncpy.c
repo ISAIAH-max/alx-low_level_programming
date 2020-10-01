14a7d9be115d2572b60df3d7ac81ef69792f5d07
ISAIAH-max <mlewaisaiah@gmail.com>
2020-10-01 18:48:50 +0300

    A function that copies a string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ *_strncpy - A function that copies a string
+ *@dest: Copied String
+ *@src: The String to be copied
+ *@n: Iterator
+ *Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+int x;
+
+for (x = 0; src[x] != '\0' && x < n; x++)
+{
+dest[x] = src[x];
+}
+
+for (x = 0; x < n; x++)
+{
+dest[x] = src[x];
+}
+return (dest);
+}
