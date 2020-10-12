42eeebdda20c7de0f3666ad5d1699cfacdb3e2f7
ISAIAH-max <mlewaisaiah@gmail.com>
2020-10-12 05:01:09 +0300

    A program that adds positive numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+#include "holberton.h"
+
+/**
+ * main - Afunction that dds positive numbers
+ * @argc: Size of array
+ * @argv: Array pointer
+ * Return: Always (0)
+ */
+
+int main(int argc, char *argv[])
+{
+int a, b, sum;
+
+sum = 0;
+
+for (a = 1; a < argc; a++)
+{
+for (b = 0; argv[a][b] != '\0'; b++)
+{
+if (!isdigit(argv[a][b]))
+{
+return (puts("Error"), 1);
+}
+}
+sum += atoi(argv[a]);
+}
+return (printf("%d\n", sum), 0);
+}
