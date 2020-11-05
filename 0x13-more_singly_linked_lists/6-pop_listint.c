cf1a616306780fcfc66aac0512e0728f11a9a606
ISAIAH-max <mlewaisaiah@gmail.com>
2020-11-05 19:11:59 +0300

    A  function that deletes the head node of a listint_t linked list

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ *pop_listint- A function that delete the head of LinkedList
+ *@head:A double pointer that points the head of a LinkedList
+ *Return:the head node’s data (n).
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *node;
+	int x;
+
+	if (head == NULL || *head == NULL)
+		return (0);
+	node = *head;
+	*head = node->next;
+	x = node->n;
+	node = NULL;
+	return (x);
+
+}
