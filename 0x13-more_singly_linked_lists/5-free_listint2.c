6db098a751f2fff5f01eac64f6d2215533995318
ISAIAH-max <mlewaisaiah@gmail.com>
2020-11-05 18:30:06 +0300

    A function that frees a listint_t list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -3,7 +3,8 @@
 /**
  *free_listint2-A function that frees a listint_t list.
  *@head:A double pointer that points the head of a LinkedList
- */
+ *Return:Void
+*/
 
 void free_listint2(listint_t **head)
 {
