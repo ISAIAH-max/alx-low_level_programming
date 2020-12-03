193d6616b16f685f2755963f9aa3fd13f8247722
ISAIAH-max <mlewaisaiah@gmail.com>
2020-12-03 14:01:12 +0300

    A program that generates random valid passwords for the program 101-crackme

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ *main - Generates random valid passwords for the
+ *program 101-crackme.
+ *Return: Always 0.
+ */
+
+int main(void)
+{
+	int myrand;
+	int count;
+	int total;
+
+	srand(time(NULL));
+	for (count = 0, total = 2772; total > 122; count++)
+	{
+		myrand = (rand() % 125) + 1;
+		printf("%c", myrand);
+		total -= myrand;
+	}
+	printf("%c", total);
+
+	return (0);
+}
