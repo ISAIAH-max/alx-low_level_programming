08eaa266d95bf917efd84a132c356c5fc3c92be6
ISAIAH-max <mlewaisaiah@gmail.com>
2021-01-30 02:00:52 +0300

    Add node

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -6,7 +6,7 @@
  *add_dnodeint-A pointer function that adds a new node at the beginning
  *of a dlistint_t list
  *@head:A double pointer to the head
- *@n-The integer add at the beginning of the list
+ *@n:The integer add at the beginning of the list
  *
  *Return:the address of the new element, or NULL if it failed
  */
