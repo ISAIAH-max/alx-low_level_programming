d7e26da029edef5e9899346ae324c9dcddf77bfc
ISAIAH-max <mlewaisaiah@gmail.com>
2021-01-30 01:40:55 +0300

    List length

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ *dlistint_len-function that returns the number of elements
+ *in a linked dlistint_t list
+ *@h:Dlink list header
+ *
+ *Return:the number of elements 
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t i;
+
+        for(i = 0; h != NULL; i++)
+	{
+		h = h->next;
+	}
+	return (i);
+}
