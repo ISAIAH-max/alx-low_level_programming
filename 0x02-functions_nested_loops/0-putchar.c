cbc55223d2b0eb10709bf014ddced83028fb4703
ISAIAH-max <mlewaisaiah@gmail.com>
2023-03-05 14:43:13 -0800

    a program that prints _putchar, followed by a new line

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ *main - Program that prints Holberton
+ *Return: Always zero(success)
+ */
+
+int main(void)
+{
+char *school_name = "_putchar";
+int letters = 0;
+
+for (letters = 0; letters <= 8; letters++)
+{
+_putchar(school_name[letters]);
+}
+_putchar('\n');
+
+return (0);
+}
