3e660fe82596c367cadc245424ce23abdf1c7e22
isaiah-max <mlewaisaiah@gmail.com>
2024-07-29 20:56:59 +0000

    Update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,11 +20,19 @@ int create_file(const char *filename, char *text_content)
 		}
 	}
 	f_open = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	f_write = write(f_open, text_content, length);
+	if (f_open == -1)
+	        return (-1);
 
-	if (f_open == -1 || f_write == -1)
-		return (-1);
-
+	if (text_content != NULL)
+	{
+	        f_write = write(f_open, text_content, length);
+		if (f_write == -1)
+		{
+		        close(f_open);
+		        return (-1);
+		}
+	  
+	}
 	close(f_open);
 
 	return (1);
