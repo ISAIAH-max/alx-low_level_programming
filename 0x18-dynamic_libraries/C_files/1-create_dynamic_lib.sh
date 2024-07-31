716cce16d06149e282c8414202f42ad49dff1d81
isaiah-max <mlewaisaiah@gmail.com>
2024-07-31 20:11:11 +0000

    Update

--- 0x18-dynamic_libraries/C_files/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/C_files/1-create_dynamic_lib.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# script that creates a dynamic library called liball.so from all the .c files that are in the current directory.
+# script that creates a dynamic library called libdynamic.so from all the .c files that are in the current directory.
 
-gcc -Wall -Werror -Wextra -pedantic -fPIC *.c -shared -o liball.so
+gcc -Wall -Werror -Wextra -pedantic -fPIC *.c -shared -o libdynamic.so
